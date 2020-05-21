2d27acf12ec41db1b856dd79c53c4be2562a88dd
Manuel172 <manuelsalasc@hotmail.com>
2020-05-20 22:01:36 -0400

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿
+## HTML5 con Css3, Ejemplo responsivo
+
+Este es un ejemplo de sitio responsivo a los dispositivos según tamaño de pantalla
+* sitio subido a http://tuterrarium.byethost22.com
+
+
+
---
+++ index.js
@@ -0,0 +1,28 @@
+function contenido1() {
+    document.getElementById('cont1').style.display = 'block';
+    document.getElementById('cont2').style.display = 'none';
+    document.getElementById('cont3').style.display = 'none';
+    document.getElementById('cont4').style.display = 'none';
+}
+
+function contenido2() {
+    document.getElementById('cont1').style.display = 'none';
+    document.getElementById('cont2').style.display = 'block';
+    document.getElementById('cont3').style.display = 'none';
+    document.getElementById('cont4').style.display = 'none';
+
+}
+
+function contenido3() {
+    document.getElementById('cont1').style.display = 'none';
+    document.getElementById('cont2').style.display = 'none';
+    document.getElementById('cont3').style.display = 'block';
+    document.getElementById('cont4').style.display = 'none';
+}
+
+function contenido4() {
+    document.getElementById('cont1').style.display = 'none';
+    document.getElementById('cont2').style.display = 'none';
+    document.getElementById('cont3').style.display = 'none';
+    document.getElementById('cont4').style.display = 'block';
+}
